359680132de42803290c4d03041b9217131e91dd
Nicolae Darie-Nistor <memedarie@gmail.com>
2023-10-25 17:50:52 +0200

    Initial Commit

---
+++ Gardenia/GardeniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardeniaApp.swift
+//  Gardenia
+//
+//  Created by Darie-Nistor Nicolae on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct GardeniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
